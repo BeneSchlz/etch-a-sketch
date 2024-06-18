ac38f0330244abe78d4f8771968e2546d2c917f2
BeneSchlz <123456789+odin@bschulz735@gmail.com>
2024-06-18 18:49:50 +0200

    created div with js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-const div = document.getElementById("div");
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+
+container.appendChild(content);
 
