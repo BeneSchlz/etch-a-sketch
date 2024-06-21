d5f449e8e89c517f1bc80ba4294cae77ea3537d4
BeneSchlz <123456789+odin@bschulz735@gmail.com>
2024-06-21 18:37:04 +0200

    Changed Approach

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
 const container = document.querySelector("#container");
- for(i = 0; i < 256; i++){
-    const content = document.createElement("div");
-content.classList.add("content");
-content.textContent = "!";
 
+createGrid = (rows, cols) =>{
+    container.innerHTML = "";
+
+    const cellSize =  `${100 / cols}%`;
+
+ for(let i = 0; i < rows * cols; i++) {
+    const content = document.createElement("div");
+content.classList.add("content-grid");
+
+content.style.width = cellSize;
+content.style.height = cellSize;
+
+content.addEventListener("mousemove", () => {
+    content.style.backgroundColor = "black";
+});
 
 container.appendChild(content);
 }
+};
+
+document.querySelector("#gridButton").addEventListener("click", () =>{
+    const gridSize = prompt("Enter size of grid");
+    if (gridSize !== null && gridSize > 0 && gridSize <= 100){
+        createGrid(gridSize, gridSize);
+    }
+});
+
+createGrid(16, 16);
 
